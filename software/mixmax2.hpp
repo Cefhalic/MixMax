c381c54f99026af4cbfb8aa181f7a4f901415173
arose <arose@cern.ch>
2022-11-01 17:29:48 +0000

    Move software from counter to flag

--- software/mixmax2.hpp
+++ software/mixmax2.hpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include "signal.hpp"
 
-// #include <iostream>
+#include <iostream>
 // #include <iomanip>
 
 constexpr uint64_t M61( 0x1FFFFFFFFFFFFFFF );
@@ -21,11 +21,12 @@ inline uint64_t MOD_MERSENNE( const uint64_t& aVal )
 struct tRngState
 {
   signal< uint64_t> W[ 16 ];
-  int counter;
+  signal< bool > flag[ 19 ];
+  signal< bool > run;
   signal< uint64_t> PartialSumOverOld , SumOverNew , RotatedPreviousPartialSumOverOld , PreSum;
 
-  tRngState() : W{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 } ,
-  counter( -3 ) , PartialSumOverOld( 0 ) , SumOverNew( 1 ) , RotatedPreviousPartialSumOverOld( 0 ) , PreSum( 0 )
+  tRngState() : W{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 } , flag{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } , run( 0 ) ,
+  PartialSumOverOld( 0 ) , SumOverNew( 1 ) , RotatedPreviousPartialSumOverOld( 0 ) , PreSum( 0 )
   {}
 
 
@@ -40,15 +41,20 @@ struct tRngState
 
     for( int i(0); i!=15; ++i ) W[ i + 1 ] = W[ i ];
 
+    flag[0] = 0;
+    for( int i(0); i!=18; ++i ) flag[ i + 1 ] = flag[ i ];
+    flag[3] = *flag[2] | *flag[18];
+    if( *flag[2] ) run = 1;
+
+
     // ===================================================================================
     // Two clock-cycles ahead
-    auto nextcounter = (counter+2) % 16;
-    if( nextcounter == 0 )
+    if( *flag[1] || *flag[17] )
     {
       RotatedPreviousPartialSumOverOld = 0;
       PartialSumOverOld = W[13];
     }
-    else if( nextcounter > 0 )
+    else
     {
       RotatedPreviousPartialSumOverOld = Rotate_61bit( *PartialSumOverOld , 36 );
       PartialSumOverOld = MOD_MERSENNE( *PartialSumOverOld + *W[13] ); 
@@ -64,19 +70,18 @@ struct tRngState
     // Current clock
     auto Temp = *SumOverNew + *PreSum; // Variable
 
-    if( counter == 0 )
+    if( *flag[3] )
     {
       W[0] = RetVal = MOD_MERSENNE( Temp );
       SumOverNew = MOD_MERSENNE( *SumOverNew + Temp );
     }
-    else if( counter > 0 )
+    else if( *run )
     { 
       W[0] = RetVal = MOD_MERSENNE( *W[0] + *PreSum );
       SumOverNew = MOD_MERSENNE( *W[0] + Temp );
     }
     // ===================================================================================
 
-    counter = (counter+1) % 16;
     return RetVal;
   }
 
