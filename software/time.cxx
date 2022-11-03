fa206fc41a5fff2ea89a48d7256ea5d0cffb1ab9
arose <arose@cern.ch>
2022-11-03 12:58:20 +0000

    Move from high_resolution_clock to steady_clock

--- software/time.cxx
+++ software/time.cxx
@@ -12,9 +12,9 @@ void TimeOriginal( const uint64_t& aIterations )
   rng_state_t lStateOrig{ {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1} , 1 , 2 };
   lStateOrig.sumtot = iterate_raw_vec( lStateOrig.V , lStateOrig.sumtot );
 
-  auto start = high_resolution_clock::now();
+  auto start = steady_clock::now();
   for( uint64_t i(0) ; i != aIterations ; ++i ) flat( &lStateOrig );
-  auto stop = high_resolution_clock::now(); 
+  auto stop = steady_clock::now(); 
   auto duration = duration_cast<nanoseconds>(stop - start).count();
   std::cout << "Original  | " << aIterations << " iterations | " << duration << "ns | " << double(duration)/aIterations << "ns/it" << std::endl;
 }
@@ -23,9 +23,9 @@ void TimeCleaned( const uint64_t& aIterations )
 {
   clean::rng_state_t lStateClean;
 
-  auto start = high_resolution_clock::now();
+  auto start = steady_clock::now();
   for( uint64_t i(0) ; i != aIterations ; ++i ) lStateClean.get();
-  auto stop = high_resolution_clock::now(); 
+  auto stop = steady_clock::now(); 
   auto duration = duration_cast<nanoseconds>(stop - start).count();
   std::cout << "Cleaned 1 | " << aIterations << " iterations | " << duration << "ns | " << double(duration)/aIterations << "ns/it" << std::endl;
 }
@@ -34,9 +34,9 @@ void TimeCleaned2( const uint64_t& aIterations )
 {
   clean::rng_state_t lStateClean;
 
-  auto start = high_resolution_clock::now();
+  auto start = steady_clock::now();
   for( uint64_t i(0) ; i != aIterations ; ++i ) lStateClean.get2();
-  auto stop = high_resolution_clock::now(); 
+  auto stop = steady_clock::now(); 
   auto duration = duration_cast<nanoseconds>(stop - start).count();
   std::cout << "Cleaned 2 | " << aIterations << " iterations | " << duration << "ns | " << double(duration)/aIterations << "ns/it" << std::endl;
 }
